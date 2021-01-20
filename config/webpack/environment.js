28ca42726ffa0dde092f2a7c0c0386092b35ffff
Yoshihiro Kimura <kimuraysp@gmail.com>
2021-01-20 19:22:25 +0900

    jqueryを使えるようにした

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+// 参考: https://qiita.com/jerrywdlee/items/cfd06a64edffd8703a1e
+const webpack = require("webpack");
 
-module.exports = environment
+environment.config.externals = {
+  jquery: "$",
+};
+
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    "window.$": "jquery",
+  })
+);
+
+module.exports = environment;
